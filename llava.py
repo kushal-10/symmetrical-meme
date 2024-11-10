e466dc8a41f68474c86dd53c6860a00e864b01fe
kushal-10 <kushalkoshti11@gmail.com>
2024-11-10 11:45:38 +0100

    test with tokenizer

--- llava.py
+++ llava.py
@@ -29,7 +29,8 @@ prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
 
 image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
 raw_image = Image.open(requests.get(image_file, stream=True).raw)
-inputs = processor(images=None, text=prompt, return_tensors='pt').to(0, torch.float16)
+# inputs = processor(images=None, text=prompt, return_tensors='pt').to(0, torch.float16)
+inputs = processor.tokenizer(text=prompt, return_tensors="pt").to(0, torch.float16)
 
 output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
 print(processor.decode(output[0][2:], skip_special_tokens=True))
