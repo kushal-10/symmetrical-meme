201cf6fb8109143257783edd9b4011e7b3db979d
kushal-10 <kushalkoshti11@gmail.com>
2024-11-10 11:43:37 +0100

    check text only

--- llava.py
+++ llava.py
@@ -21,8 +21,7 @@ conversation = [
 
       "role": "user",
       "content": [
-          {"type": "text", "text": "What are these?"},
-          {"type": "image"},
+          {"type": "text", "text": "What are these?"}
         ],
     },
 ]
@@ -30,7 +29,7 @@ prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
 
 image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
 raw_image = Image.open(requests.get(image_file, stream=True).raw)
-inputs = processor(images=raw_image, text=prompt, return_tensors='pt').to(0, torch.float16)
+inputs = processor(images=None, text=prompt, return_tensors='pt').to(0, torch.float16)
 
 output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
 print(processor.decode(output[0][2:], skip_special_tokens=True))
