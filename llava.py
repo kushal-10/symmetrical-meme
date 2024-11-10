06b188378ee02948df95bc5f2c7930c9dc754067
kushal-10 <kushalkoshti11@gmail.com>
2024-11-10 12:13:21 +0100

    update template

--- llava.py
+++ llava.py
@@ -36,7 +36,7 @@ formatted_prompt = chat_template.render(messages=conversation)
 image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
 raw_image = Image.open(requests.get(image_file, stream=True).raw)
 # inputs = processor(images=None, text=prompt, return_tensors='pt').to(0, torch.float16)
-inputs = processor(text=formatted_prompt, images = [] return_tensors="pt").to("cuda")
+inputs = processor(text=formatted_prompt, images = [], return_tensors="pt").to("cuda")
 
 output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
 print(processor.decode(output[0][2:], skip_special_tokens=True))
