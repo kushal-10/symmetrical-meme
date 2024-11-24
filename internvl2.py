745b5bb176853fe20de0e4156a195c1ae0c2c715
kushal-10 <kushalkoshti11@gmail.com>
2024-11-24 12:55:51 +0100

    test single image multi round for internvl models

--- internvl2.py
+++ internvl2.py
@@ -4,6 +4,8 @@ import torchvision.transforms as T
 from PIL import Image
 from torchvision.transforms.functional import InterpolationMode
 from transformers import AutoModel, AutoTokenizer
+import requests
+from io import BytesIO
 
 IMAGENET_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_STD = (0.229, 0.224, 0.225)
@@ -72,7 +74,12 @@ def dynamic_preprocess(image, min_num=1, max_num=12, image_size=448, use_thumbna
     return processed_images
 
 def load_image(image_file, input_size=448, max_num=12):
-    image = Image.open(image_file).convert('RGB')
+    if image_file.startswith("http"):
+        response = requests.get(image_file)
+        image = Image.open(BytesIO(response.content)).convert('RGB')
+    else:
+        image = Image.open(image_file).convert('RGB')
+
     transform = build_transform(input_size=input_size)
     images = dynamic_preprocess(image, image_size=input_size, use_thumbnail=True, max_num=max_num)
     pixel_values = [transform(image) for image in images]
@@ -123,47 +130,13 @@ tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True, use_fast
 pixel_values = load_image('./examples/image1.jpg', max_num=12).to(torch.bfloat16).cuda()
 generation_config = dict(max_new_tokens=1024, do_sample=True)
 
-# multi-image multi-round conversation, combined images (多图多轮对话，拼接图像)
-pixel_values1 = load_image('./examples/image1.jpg', max_num=12).to(torch.bfloat16).cuda()
-pixel_values2 = load_image('./examples/image2.jpg', max_num=12).to(torch.bfloat16).cuda()
-pixel_values = torch.cat((pixel_values1, pixel_values2), dim=0)
-
-question = '<image>\nDescribe the two images in detail.'
+# Single image multi round conversation
+question = '<image>\nDescribe the image in detail.'
 response, history = model.chat(tokenizer, pixel_values, question, generation_config,
                                history=None, return_history=True)
 print(f'User: {question}\nAssistant: {response}')
 
-print("-"*100)
-print("History: ")
-print("-"*100)
-print(history)
-
-question = 'What are the similarities and differences between these two images.'
-response, history = model.chat(tokenizer, pixel_values, question, generation_config,
+question = 'Explain more about this image.'
+response, history = model.chat(tokenizer, None, question, generation_config,
                                history=history, return_history=True)
 print(f'User: {question}\nAssistant: {response}')
-
-print("-"*100)
-print("History: ")
-print("-"*100)
-print(history)
-
-
-# multi-image multi-round conversation, separate images (多图多轮对话，独立图像)
-pixel_values1 = load_image('./examples/image1.jpg', max_num=12).to(torch.bfloat16).cuda()
-pixel_values2 = load_image('./examples/image2.jpg', max_num=12).to(torch.bfloat16).cuda()
-pixel_values = torch.cat((pixel_values1, pixel_values2), dim=0)
-num_patches_list = [pixel_values1.size(0), pixel_values2.size(0)]
-
-question = 'Image-1: <image>\nImage-2: <image>\nDescribe the two images in detail.'
-response, history = model.chat(tokenizer, pixel_values, question, generation_config,
-                               num_patches_list=num_patches_list,
-                               history=None, return_history=True)
-print(f'User: {question}\nAssistant: {response}')
-
-question = 'What are the similarities and differences between these two images.'
-response, history = model.chat(tokenizer, pixel_values, question, generation_config,
-                               num_patches_list=num_patches_list,
-                               history=history, return_history=True)
-print(f'User: {question}\nAssistant: {response}')
-
